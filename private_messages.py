31df70e9f7c9d4648b03adfc40999a834d20808d
yugene <yugene1986@gmail.com>
2018-06-09 20:14:03 +0200

    fixed issue with commit through delete old and add new (damn!)

--- private_messages.py
+++ private_messages.py
@@ -10,9 +10,15 @@ def private_messages(bot, update):
     if listener:
         user = update.message.from_user.username
         userdata = session.query(GroupsMessagesListener).filter_by(username=user).first()
-        array_of_listeners = list(userdata.listeners[listener.group(1)])
+        if userdata:
+            array_of_listeners = list(userdata.listeners[listener.group(1)])
+            session.delete(userdata)
+        else:
+            array_of_listeners = list()
         array_of_listeners.append(listener.group(2))
-        userdata.listeners[listener.group(1)] = array_of_listeners
-        print("updated listener " + str(userdata.listeners[listener.group(1)]))
+
+        chat_dictionary = {listener.group(1): array_of_listeners}
+        newuserdata = GroupsMessagesListener(username=user, listeners=chat_dictionary, update_nr=1)
+
+        session.add(newuserdata)
         session.commit()
-        print("updated listener " + str(userdata.listeners[listener.group(1)]))
